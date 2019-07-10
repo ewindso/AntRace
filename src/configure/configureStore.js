ba3d33e0f780862d03d632946cf027bd0c4a5f96
Elijah Windsor <ewindsor@gmail.com>
2019-07-10 17:28:20 -0400

    Persisting auth state

--- src/configure/configureStore.js
+++ src/configure/configureStore.js
@@ -1,11 +1,21 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import { createLogger } from 'redux-logger'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import { watchAuthRequests } from '../sagas/auth'
 import auth from '../reducers/auth'
 import ant from '../reducers/ant'
 
 const reducers = combineReducers({ auth, ant })
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth']
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
 
 const logger = createLogger({})
 const sagaMiddleware = createSagaMiddleware()
@@ -14,16 +24,18 @@ let store
 
 if(__DEV__) {
   store = createStore(
-    reducers, 
+    persistedReducer, 
     applyMiddleware(logger, sagaMiddleware)
   )
 } else {
   store = createStore(
-    reducers,
+    persistedReducer,
     applyMiddleware(sagaMiddleware)
   )
 }
 
 sagaMiddleware.run(watchAuthRequests)
+
+const persistor = persistStore(store)
 
 export default store
