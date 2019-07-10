23969c2af54fd86299964ff179fdd05e91519652
Elijah Windsor <ewindsor@gmail.com>
2019-07-10 17:05:18 -0400

    Added auth reducer

---
+++ src/reducers/auth.js
@@ -0,0 +1,23 @@
+import {
+  LOGIN_USER, 
+  LOGOUT_USER
+} from '../constants/actionTypes'
+
+const initialState = {
+  loggedIn: false
+}
+
+export function auth(state=initialState, action) {
+  const { type, payload } = action 
+
+  switch(type) {
+    case LOGIN_USER: {
+      return { ...state, loggedIn: true }
+    }
+    case LOGOUT_USER: {
+      return { ...state, loggedIn: false }
+    }
+  }
+
+  return state 
+}
