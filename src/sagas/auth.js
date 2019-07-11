f6df4aa4616c8492757b921a242124ebe8f57435
Elijah Windsor <ewindsor@gmail.com>
2019-07-11 08:39:07 -0400

    Started handling rehydration / login / logout

--- src/sagas/auth.js
+++ src/sagas/auth.js
@@ -5,7 +5,26 @@ import { LOGIN_USER, LOGOUT_USER } from '../constants/actionTypes'
 export function *watchAuthRequests() {
   let action 
 
-  while(action = yield take([LOGIN_USER, LOGOUT_USER])) {
-    console.log(action)
+  while(action = yield take(['persist/REHYDRATE', LOGIN_USER, LOGOUT_USER])) {
+    const { type } = action 
+
+    switch(type) {
+      case 'persist/REHYDRATE':
+      case LOGIN_USER:
+      case LOGOUT_USER: {
+        yield fork(checkUserLoggedIn)
+        break 
+      }
+    }
   }
+}
+
+function *checkUserLoggedIn() {
+  const {
+    auth: {
+      loggedIn
+    }
+  } = yield select() 
+
+  console.log(loggedIn)
 }
