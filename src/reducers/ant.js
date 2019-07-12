1836e4afed0d1bf9a2b77696fdaecfb99d50daa9
Elijah Windsor <ewindsor@gmail.com>
2019-07-12 13:20:14 -0400

    Handling UPDATE_ANTS_DATA, setting isLoading to false

--- src/reducers/ant.js
+++ src/reducers/ant.js
@@ -1,9 +1,18 @@
+import { UPDATE_ANTS_DATA } from '../constants/actionTypes'
+
 const initialState = {
   data: [], 
-  isLoading: false, 
+  isLoading: true, 
 }
 
 export default function ant(state=initialState, action) {
+  const { type, payload } = action 
+
+  switch(type) {
+    case UPDATE_ANTS_DATA: {
+      return { ...state, data: payload.data, isLoading: false }
+    }
+  }
 
   return state
 }
