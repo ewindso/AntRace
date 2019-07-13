410549c846265514464cc72dc9e670aa0706b102
Elijah Windsor <ewindsor@gmail.com>
2019-07-13 00:10:09 -0400

    Started doing ant computations

--- src/sagas/ant.js
+++ src/sagas/ant.js
@@ -10,9 +10,53 @@ export function* watchAntRequests() {
 
     switch(type) {
       case START_ANT_COMPUTATIONS: {
-        
+        yield fork(startAntComputations)
         break 
       }
     }
   }
+}
+
+function* startAntComputations() {
+  const {
+    ant: {
+      data
+    }
+  } = yield select() 
+
+  const channel = yield call(antCalculator, data)
+  let antsLeftToCompute = data.length 
+
+  while(antsLeftToCompute > 0) {
+    const { antName, likelihood } = yield take(channel)
+    console.log(antName, likelihood)
+    antsLeftToCompute--
+  }
+
+  channel.close()
+}
+
+function antCalculator(antsData) {
+  return eventChannel(emit => {
+
+    antsData.forEach(antData => {
+      const calc = generateAntWinLikelihoodCalculator()
+      calc(likelihood => {
+        emit({ antName: antData.name, likelihood })
+      })
+    })
+
+    return () => {}
+  })
+}
+
+function generateAntWinLikelihoodCalculator() {
+  const delay = 7000 + Math.random() * 7000;
+  const likelihoodOfAntWinning = Math.random();
+
+  return (callback) => {
+    setTimeout(() => {
+      callback(likelihoodOfAntWinning);
+    }, delay);
+  };
 }
