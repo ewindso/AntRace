3e4042989787cdb206a3696ec6ed4831037838b8
Elijah Windsor <ewindsor@gmail.com>
2019-07-12 23:47:47 -0400

    Started layout of Main

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { SafeAreaView, View, Text, StyleSheet, TouchableOpacity, LayoutAnimation, FlatList, Image, ActivityIndicator } from 'react-native'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import Ant from '../components/Ant'
 
 const Main = React.memo(props => {
   useEffect(() => {
@@ -18,15 +19,26 @@ const Main = React.memo(props => {
           <ActivityIndicator animating={true} size={'large'} />
         </View>
       ) : (
-        <View key={'container'}>
+        <View key={'container'} style={styles.container}>
           <View style={styles.header}>
             <TouchableOpacity style={styles.logoutButton} onPress={() => {}}>
               <Text style={styles.logoutButtonText}>Logout</Text>
             </TouchableOpacity>
           </View>
-          <View style={styles.antView}>
-            <Image source={require('../assets/ant.png')} style={{tintColor: 'orange', width: 50, height: 30}} />
+          <View style={styles.listView}>
           </View>
+          <View style={styles.infoView}>
+            <TouchableOpacity style={styles.startButton}>
+              <Text style={styles.startButtonText}>Start Computation</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.animationView}>
+
+          </View>
+          {/* <View style={styles.antView}>
+            <Image source={require('../assets/ant.png')} style={{tintColor: 'orange', width: 50, height: 30}} />
+            <Ant />
+          </View> */}
         </View>
       )}
     </SafeAreaView>
@@ -59,6 +71,30 @@ const styles = StyleSheet.create({
   logoutButtonText: {
     fontSize: 18, 
     color: 'white'
+  }, 
+  listView: {
+    flex: 0.5
+  }, 
+  infoView: {
+    borderTopWidth: 1, 
+    borderTopColor: '#ccc', 
+    borderBottomWidth: 1, 
+    borderBottomColor: '#ccc',
+    height: 60,
+    justifyContent: 'center', 
+    alignItems: 'center'
+  }, 
+  startButton: {
+    height: 44, 
+    width: 200, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    borderWidth: 0.25, 
+    borderColor: 'black', 
+    borderRadius: 10
+  }, 
+  animationView: {
+    flex: 1
   }
 })
 
