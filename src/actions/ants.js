ff7e88489df287c8e2f428eacdb7a4d1a4da6ba3
Elijah Windsor <ewindsor@gmail.com>
2019-07-13 00:15:22 -0400

    Added updateAntComputation action creator

--- src/actions/ants.js
+++ src/actions/ants.js
@@ -1,7 +1,13 @@
 import {
-  START_ANT_COMPUTATIONS
+  START_ANT_COMPUTATIONS, 
+  UPDATE_ANT_COMPUTATION, 
+  ANT_COMPUTATIONS_FINISHED,
 } from '../constants/actionTypes'
 
 export function startAntComputations() {
   return { type: START_ANT_COMPUTATIONS }
+}
+
+export function updateAntComputation(name, likelihood) {
+  return { type: UPDATE_ANT_COMPUTATION, payload: { name, likelihood } }
 }
