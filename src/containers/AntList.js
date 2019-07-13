864e45cc57006631da861ab93957257c362859e0
Elijah Windsor <ewindsor@gmail.com>
2019-07-13 17:29:12 -0400

    Started rendering rows, switched to ScrollView to ensure all rows render

--- src/containers/AntList.js
+++ src/containers/AntList.js
@@ -1,26 +1,66 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text, StyleSheet, TouchableOpacity, LayoutAnimation, FlatList, Image, ActivityIndicator } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, LayoutAnimation, ScrollView, Image, ActivityIndicator } from 'react-native'
+import Ant from '../components/Ant'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 const AntList = React.memo(props => {
+  const { data, computations } = props 
+  
   return (
-    <FlatList 
+    <ScrollView 
       style={styles.container}
-      data={[]}
-      renderItem={({item}) => <View></View>}
-    />
+    >
+      {data.map(item => {
+        const { name, color } = item 
+        const antComputation = computations[name]
+        console.log(antComputation)
+        return (
+          <View key={name} style={styles.row}>
+            <Text style={styles.nameText}>{name}</Text>
+            <Text style={styles.statusText}>In Progress</Text>
+            <View style={styles.antView}>
+              <Ant color={color} duration={5000} />
+            </View>
+          </View>
+        )
+      })}
+    </ScrollView>
   )
 })
 
 const styles = StyleSheet.create({
   container: {
     flex: 1, 
+  }, 
+  row: {
+    height: 90,  
+    justifyContent: 'center', 
+    paddingLeft: 20, 
+    borderBottomWidth: 0.5, 
+    borderBottomColor: '#ccc'
+  }, 
+  nameText: {
+    fontSize: 18, 
+  }, 
+  statusText: {
+    fontSize: 15, 
+    paddingTop: 20
+  }, 
+  antView: {
+    position: 'absolute'
   }
 })
 
 const mapStateToProps = state => {
-  return {}
+  const {
+    ant: {
+      data, 
+      computations,
+    }, 
+  } = state 
+
+  return { data, computations }
 }
 
 const mapDispatchToProps = dispatch => {
