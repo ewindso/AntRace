d20488bd07ad2c62c069316d55adba7554cc6ecb
Elijah Windsor <ewindsor@gmail.com>
2019-07-14 18:37:38 -0400

    Enabling LayoutManager animations on Android

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-
+import { UIManager, Platform } from 'react-native';
 import { store, persistor } from './src/configure/configureStore'
 import Home from './src/containers/Home'
+
+if(Platform.OS === 'android') {
+  console.log('ANDROID')
+  UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true)
+}
 
 const App = () => {
   return (
